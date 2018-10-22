db0196c82c5e36ba29aeba6ff9787e70d7e2eeb5
mihai <mihai@eessidev24>
2018-10-22 21:39:38 +0200

    Clear before reloading

--- ProjectReferences/ProjectReferences/Main.cs
+++ ProjectReferences/ProjectReferences/Main.cs
@@ -9,11 +9,16 @@ namespace ProjectReferences
             InitializeComponent();
         }
 
-        private void Main_DragDrop(object sender, DragEventArgs e)
+        private void Clear()
         {
-            var files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            
-            foreach (var project in files)
+            hierarchicalReferences.Nodes.Clear();
+            flatReferences.Nodes.Clear();
+            folderStructure.Nodes.Clear();
+        }
+
+        private void Display(string[] projects)
+        {
+            foreach (var project in projects)
             {
                 hierarchicalReferences.Nodes.Add(Dependency.Hierarchy(project));
                 flatReferences.Nodes.Add(Dependency.FlatList(project));
@@ -21,10 +26,20 @@ namespace ProjectReferences
             }
         }
 
+        private void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            var projects = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            Clear();
+            Display(projects);
+        }
+
         private void Main_DragEnter(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))   
-                e.Effect = DragDropEffects.All; 
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
         }
     }
 }
