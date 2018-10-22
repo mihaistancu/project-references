2c9b958f8d38e3cfc270bd2ba7b8552167f6a423
mihai <mihai@eessidev24>
2018-10-22 14:50:27 +0200

    Add first-level references

--- ProjectReferences/ProjectReferences/Main.cs
+++ ProjectReferences/ProjectReferences/Main.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace ProjectReferences
 {
@@ -16,7 +19,14 @@ namespace ProjectReferences
             
             foreach (var project in files)
             {
-                treeView1.Nodes.Add(Path.GetFileNameWithoutExtension(project));
+                var node = treeView1.Nodes.Add(Path.GetFileNameWithoutExtension(project));
+
+                var references = References(project);
+
+                foreach (var reference in references)
+                {
+                    node.Nodes.Add(Path.GetFileNameWithoutExtension(reference));
+                }
             }
         }
 
@@ -24,6 +34,13 @@ namespace ProjectReferences
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop, false))   
                 e.Effect = DragDropEffects.All; 
+        }
+        
+        private List<string> References(string projectPath)
+        {
+            return XDocument.Load(projectPath)
+                .Descendants().Where(e => e.Name.LocalName == "ProjectReference")
+                .Select(e=>e.Attribute("Include").Value).ToList();
         }
     }
 }
